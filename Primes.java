82c2c5bc3db1d3bbd5287d9b8c6478179c49fc06
omer-argaman <omer.argaman@post.runi.ac.il>
2024-11-28 13:52:00 +0200

    Second question version 4

--- Primes.java
+++ Primes.java
@@ -2,8 +2,8 @@ public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         System.out.println("Prime numbers up to " + n + ":");
-        boolean[] primes = new boolean[n]; // creates a new array type boolean with length of n
-        for (int i = 0; i < n; i++) {
+        boolean[] primes = new boolean[n+1]; // creates a new array type boolean with length of n
+        for (int i = 0; i < n + 1; i++) {
             primes[i] = true; // initializes all elements in the array to true
         }
         primes[0] = false; //
@@ -11,7 +11,7 @@ public class Primes {
         int counter = 0; 
         int div = 2; // initial
         while (div < Math.sqrt(n)) {
-            while (counter < n) {
+            while (counter < n+1) {
                 if ( primes[counter] == true) {
                     if ( counter%div == 0 ) {
                         if (counter != div) {
@@ -26,14 +26,14 @@ public class Primes {
         div++;
         }
         double counter2 = 0;
-        for (int i2 = 0; i2 < n; i2++) {
+        for (int i2 = 0; i2 < n + 1; i2++) {
             if ( primes[i2] == true) {
                 System.out.println(i2);
                 counter2++;
             }
         }
         int x = (int)((counter2/n) * 100);
-        System.out.println("There are " + (int)counter2 + " primes between 2 and " + n + ". ("+ x +"% are primes)");
+        System.out.println("There are " + (int)counter2 + " primes between 2 and " + n + " ("+ x +"% are primes)");
         /* for (int i3 = 0; i3 < n; i3++) {
             System.out.println(primes[i3]);
         } This prints out the values of the arrey inorder to trubleshoot if needed */
